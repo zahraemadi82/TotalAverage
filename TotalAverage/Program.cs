2fb802e29fd422a8a2794cf6edf44bcf5fce8733
User <User@DESKTOP-1364VBG>
2024-01-29 17:31:55 +0330

    Add project files.

---
+++ TotalAverage/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+double sum = 0;
+double countSum = 0;
+for (int i = 1; i <= 20; i++)
+{
+    Console.Write($"Enter Number {i} : ");
+    double number = Convert.ToDouble(Console.ReadLine());
+    if (number > 20 || number < 0)
+    {
+        Console.WriteLine("Invalid input");
+    }
+    else
+    {
+        sum += number;
+        countSum += 1;
+    }
+}
+
+sum /= countSum;
+Console.WriteLine(sum);
